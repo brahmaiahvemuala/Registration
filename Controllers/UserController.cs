b6f5a946887535c823a2bd2503def07934a2ccc6
brahmaiahvemuala <brahmaiahvemuala>
2020-04-29 11:41:14 +0300

    Importing Project

---
+++ Controllers/UserController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationAndLogin.Models;
+namespace RegistrationAndLogin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Registration()
+
+        {
+            
+           
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude = "IsEmailVerified,ActivationCode")] User user)
+        {
+            bool Status = false;
+            string message = "";
+            //
+            // Model Validation 
+            if (ModelState.IsValid)
+            {
+
+                #region //Email is already Exist 
+                var isExist = IsEmailExist(user.EmailId);
+                if (isExist)
+                {
+                    ModelState.AddModelError("EmailExist", "Email already exist");
+                    return View(user);
+                }
+                #endregion
+
+                #region Generate Activation Code 
+                user.ActivationCode = Guid.NewGuid();
+                #endregion
+
+                #region  Password Hashing 
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword); //
+                #endregion
+                user.IsEmailVerified = false;
+
+                #region Save to Database
+                using (RegistrationAndLoginEntities dc = new RegistrationAndLoginEntities())
+                {
+                    dc.Users.Add(user);
+                    dc.SaveChanges();
+
+                    //Send Email to User
+                    SendVerificationLinkEmail(user.EmailId, user.ActivationCode.ToString());
+                    message = "Registration successfully done. Account activation link " +
+                        " has been sent to your email id:" + user.EmailId;
+                    Status = true;
+                }
+                #endregion
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return View(user);
+        }
+        public static class Crypto
+        {
+            public static string Hash(string value)
+            {
+                return Convert.ToBase64String(
+                    System.Security.Cryptography.SHA256.Create()
+                    .ComputeHash(Encoding.UTF8.GetBytes(value))
+                    );
+            }
+        }
+        [NonAction]
+        public bool IsEmailExist(string emailID)
+        {
+            using (RegistrationAndLoginEntities dc = new RegistrationAndLoginEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailId == emailID).FirstOrDefault();
+                return v != null;
+            }
+        }
+        [NonAction]
+        public void SendVerificationLinkEmail(string emailID, string activationCode)
+        {
+            var verifyUrl = "/User/VerifyAccount/" + activationCode;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            var fromEmail = new MailAddress("dotnetawesome@gmail.com", "Dotnet Awesome");
+            var toEmail = new MailAddress(emailID);
+            var fromEmailPassword = "********"; // Replace with actual password
+            string subject = "Your account is successfully created!";
+
+            string body = "<br/><br/>We are excited to tell you that your Dotnet Awesome account is" +
+                " successfully created. Please click on the below link to verify your account" +
+                " <br/><br/><a href='" + link + "'>" + link + "</a> ";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+               
+               
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,//
+                IsBodyHtml = true
+            })
+                smtp.Send(message);
+        }
+    }
+}
